bf864ea10169b582369d9d8671de4c88e9205b46
Knight Fu <knight.w.fu@gmail.com>
2017-12-10 20:08:59 -0500

    fixes lint issues with env.go

--- env.go
+++ env.go
@@ -1,4 +1,4 @@
-// Package that supports unmarshalling objects from environment variables by
+// Package goenv supports unmarshalling objects from environment variables by
 // defining tags appended to fields. The idea here is that there are a lot of
 // applications whose config objects are serialised from environment variable
 // values.
@@ -37,7 +37,7 @@ import (
 	"reflect"
 )
 
-// Interface for that expresses the ability to look up values from the environment
+// EnvReader is an interface for expressing the ability to look up values from the environment
 // via environment variables (LookupEnv) and the ability to query the existence of
 // many environment variables at once.
 type EnvReader interface {
@@ -52,27 +52,27 @@ type EnvReader interface {
 	HasKeys([]string) (bool, []string)
 }
 
-// An environment variable reader that implements that EnvReader interface by using the
+// OsEnvReader is an environment variable reader that implements that EnvReader interface by using the
 // os.LookupEnv method.
 type OsEnvReader struct {
 	lookup func(key string) (string, bool)
 }
 
-// Creates a new instance of OsEnvReader
+// NewOsEnvReader creates a new instance of OsEnvReader
 func NewOsEnvReader() *OsEnvReader {
 	return &OsEnvReader{
 		lookup: os.LookupEnv,
 	}
 }
 
-// Lookup a certain environment variable by name. Returns the value of the
+// LookupEnv - Lookup a certain environment variable by name. Returns the value of the
 // environment variable if the variable exists and has an assigned value. Otherwise,
 // returns an unspecific value, and the exists flag is set to false.
 func (env *OsEnvReader) LookupEnv(key string) (string, bool) {
 	return env.lookup(key)
 }
 
-// Returns whether or not a set of environment variables have corresponding
+// HasKeys - Returns whether or not a set of environment variables have corresponding
 // values along with a list of environment variables that do not have values.
 func (env *OsEnvReader) HasKeys(keys []string) (bool, []string) {
 	missingKeys := []string{}
@@ -85,19 +85,19 @@ func (env *OsEnvReader) HasKeys(keys []string) (bool, []string) {
 	return len(missingKeys) == 0, missingKeys
 }
 
-// An interface for any object that defines the UnmarshalEnv method, i.e. a
+// EnvUnmarshaler is an interface for any object that defines the UnmarshalEnv method, i.e. a
 // method that accepts an EnvReader and can unmarshal from environment variable
 // values from the EnvReader
 type EnvUnmarshaler interface {
 	UnmarshalEnv(EnvReader) error
 }
 
-// An interface for any object that implements the Unmarshal method.
+// Marshaler - An interface for any object that implements the Unmarshal method.
 type Marshaler interface {
 	Unmarshal(interface{}) error
 }
 
-// An unmarshaller that uses the DefaultParser and a specific environment reader
+// DefaultEnvMarshaler - An unmarshaller that uses the DefaultParser and a specific environment reader
 // to unmarshal primitive and derived values.
 type DefaultEnvMarshaler struct {
 	Environment EnvReader
@@ -224,7 +224,7 @@ func (marshaler *DefaultEnvMarshaler) unmarshalStruct(t reflect.Type, envPrefix 
 	return val, errors.Errorf("Cannot unmarshal non-struct type %s", tKind)
 }
 
-// Unmarshals a given value from environment variables. It accepts a pointer to a given
+// Unmarshal - Unmarshals a given value from environment variables. It accepts a pointer to a given
 // object, and either succeeds in unmarshalling the object or returns an error.
 //
 // Usage:
@@ -265,7 +265,7 @@ func (marshaler *DefaultEnvMarshaler) Unmarshal(i interface{}) error {
 	}
 
 	if t.Kind() != reflect.Struct {
-		return errors.New("Cannot unmarshal non-struct, non-EnvMarshaler objects.")
+		return errors.New("cannot unmarshal non-struct, non-EnvMarshaler objects")
 	}
 
 	val, err := marshaler.unmarshalStruct(t, "")
